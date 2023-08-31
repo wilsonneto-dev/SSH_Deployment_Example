5e6634018cc801ff048b6b6917b00b298d5a6a1e
Wilson Neto B.R <contato@wilsonneto.com.br>
2023-08-31 19:29:07 +0100

    Update index.php
    Signed-off-by: Wilson Neto B.R. <contato@wilsonneto.com.br>

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 $router = new ExpressRouter\Router();
 
-$router->get("/", function ($req, $res) { return $res->response(array("ok" => true, "tip" => "Use /articles")); });
+$router->get("/", function ($req, $res) { return $res->response(array("ok" => true, "tip" => "Use /articles", "version" => "1.0.2")); });
 
 $router->get("/articles", function ($req, $res) { return $res->response(array("example" => "list of articles")); });
 
-$router->route(isset($_GET["path"]) ? $_GET["path"] : "");
+$router->route(isset($_GET["path"]) ? $_GET["path"] : "/");
 
 ?>
